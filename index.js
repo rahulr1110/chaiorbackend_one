48d975eba60b9b93b63c4bb2bff27c8e788368cd
rahul1110 <rahulr1110@gmail.com>
2024-02-12 21:01:12 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 
 app.get("/", (req, res) => {
-  res.send("hiii");
+  res.send("hiii rahul");
 });
 app.listen(process.env.PORT, () => {
   console.log("hiiiiii");
