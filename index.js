7f0ef9dd2aad2b9853cc75f0cb3aae3ca47750be
rahul1110 <rahulr1110@gmail.com>
2024-02-12 20:54:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const express = require("express");
+const app = express();
+require('dotenv').config()
+
+app.get("/", (req, res) => {
+  res.send("hiii");
+});
+app.listen(process.env.PORT, () => {
+  console.log("hiiiiii");
+});
