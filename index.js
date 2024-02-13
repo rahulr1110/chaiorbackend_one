90900613ac1f0c8dc85571093947196d8d54eb94
rahul1110 <rahulr1110@gmail.com>
2024-02-13 06:50:03 +0530

    data file added

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const data = {
+  name: "rahul",
+  surname: "ravindran",
+};
+
 app.get("/", (req, res) => {
   res.send("hiii rahul ravindran");
+});
+app.get("/data", (req, res) => {
+  res.json(data);
 });
 app.listen(process.env.PORT, () => {
   console.log("hiiiiii");
